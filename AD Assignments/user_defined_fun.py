07dab08b63a369d862ec4deb151851e947811d93
SrikarPrasad2336 <2311cs020652@mallareddyuniversity.ac.in>
2025-01-10 10:43:50 +0530

    Delete AD Assignments/user_defined_fun.py

--- AD Assignments/user_defined_fun.py
+++
@@ -1,7 +0,0 @@
-def calculate_square(n):
-    return n * n
-
-if __name__ == "__main__":
-    n = int(input("Please enter a positive integer: "))
-    result = calculate_square(n)
-    print("The square of", n, "is:", result)
